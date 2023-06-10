8f59169d33e9e333434bbcae576a3a9963ea454a
tardisgallifrey <bigmanrunning@gmail.com>
2023-06-10 11:24:17 -0500

    Lists 06/10/2023

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+using System.Text.Encodings.Web;  //sometimes needed
 using mvcmin.Models; //included so I can use
                      //the variables in Models
 
 namespace mvcmin.Controllers;
+
+//This style of programming is called MVC.
+//Model-View-Controller
+//It works well in web app situations (and others).
+//You are in the 'C' part of MVC, a controller.
+//The controller becomes part of the web site URL.
+//In this case, the URL wil be:
+// http://www.mywebsitename.com/home
+//The 'home' on the end is the name of the controller.
+
 
 //File name of controller and name
 //of class must match
@@ -17,9 +27,24 @@ public class Home : Controller
    //This method name must match its own
    //View file name, and must be in a folder
    //that matches the controller name,
-   //in a folder called Views
+   //in a folder called Views.
+   //All controller methods must be public
+   //to be useable in the web app.
+   //To use the View() process, all
+   //controller methods must be of type:
+   //IActionResult.
+   //Other return types can be used as well,
+   //but they can't really call a View() method.
+   //Other return types must return their own data
+   //for display.
     public IActionResult Index()
     {
+        //Index is a special controller method.
+        //It allows us to use the URL of:
+        //http://www.mywebsitename.com/
+        //without needing the /home/ controller
+        //name added, at least at first.
+
         return View();
     }
 
@@ -41,7 +66,8 @@ public class Home : Controller
     {
         //I am doing it this way to be
         //patently obvious that myvar is of
-        //type Variables class.
+        //type Variables class. I could just
+        //use var myvar instead.
         Variables myvar = new Variables();
 
         //Set the num1 property field to a value
@@ -53,7 +79,26 @@ public class Home : Controller
 
         //Or, I can pass the myvar object as
         //a whole and use it this way.
-        return View(myvar);
+        //See the AddUp view for continuation.
+        //return View(myvar);
+
+        //So, we have been using the simple
+        //return View(myvar)
+        //But, you can use a conditional expression
+        //to deal with errors
+        //Change the 6 below to any other
+        //number to see the resultant error
+        return myvar.num1 == 6 ? View(myvar) : Problem("Value isn't 6.");
+    }
+
+    public IActionResult ArrayList()
+    {
+        var mylist = new Variables();
+
+        //Easy way to fill an array
+        mylist.number_array = Enumerable.Range(1, 10).ToList();
+
+        return View(mylist);
     }
 
 }
--- Models/Data.cs
+++ Models/Data.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations; //necessary
 
+
+//You are in the 'M' part of MVC.
+//These are your data models.  There can be
+//multiple data models in the folder called Models.
+//Each class should handle only one data model.
+
 namespace mvcmin.Models
 {
     //What I am showing here is the use of 
@@ -21,8 +27,19 @@ namespace mvcmin.Models
         //and assign it an auto-generated
         //getter and setter.
         //The next step occurs in the Controller
+        
         public int num1 { get; set;}
+
+        //create an empty array of integers
+        //the ? allows us to mark this as nullable.
+        //Otherwise, we would have to initialize the 
+        //array here.
+        public List<int>? number_array { get; set; }
     }
+
+    //I could create other Data Model classes.
+    //I could create other classes like Data in
+    //the Models folder.
 
     
 }
